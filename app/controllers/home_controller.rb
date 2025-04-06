e00cc67443330b05747ab4319f43ce7810e80647
DanielCzermak <czermakdaniel@gmail.com>
2025-04-06 19:40:53 +0200

    set up home page (mostly)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < ApplicationController
   def index
-    @events = Event.all
+    this_week = DateTime.now.beginning_of_day..(DateTime.now + 7.days).end_of_day
+    @events_this_week = Event.visible_to(current_user).where(
+      "start_date between ? and ?", this_week.begin, this_week.end
+    ).order(start_date: :asc)
   end
 end
